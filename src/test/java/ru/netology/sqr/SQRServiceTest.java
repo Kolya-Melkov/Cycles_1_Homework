58e51093f7fc7803d9ed649994fc4f0467e89c51
Nikolay Melkov <pestrrroy@gmail.com>
2021-11-07 23:52:11 +0300

    Naming fixed

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -2,7 +2,6 @@ package ru.netology.sqr;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
@@ -13,12 +12,11 @@ class SQRServiceTest {
                         "'Squares less than low bound', 9802, 10000, 0",
                         "'Bounds equal to the lowest square', 100, 100, 1",
                         "'Bounds equal to the highest square', 9801, 9801, 1",
-                        "'Upper bounds less than low bound', 700, 100, 0"
+                        "'Upper bound less than low bound', 700, 100, 0"
     })
-    void checkDoubles(String testName, int lowBound, int upperBound, int expected) {
+    void checkSquares(String testName, int lowBound, int upperBound, int expected) {
         SQRService service = new SQRService();
         int actual = service.countNumberOfBounds(lowBound, upperBound);
         assertEquals(expected, actual);
     }
-
 }
